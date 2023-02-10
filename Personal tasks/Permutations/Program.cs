1f7bcd8e6a6b215aa8ffece5e227f6173527ee50
Daniel Yordanov <dakataebg@students.softuni.bg>
2023-02-10 20:57:38 +0200

    Permutations generator
    I haven't solved algorithms for a while, and in school, we talked about permutations, so I implemented this program to generate them.

---
+++ Personal tasks/Permutations/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string text = "123456";
+
+        int count = Permutate(text);
+        Console.WriteLine(count);
+    }
+
+    static int Permutate(string text, int at = 0)
+    {
+        if (at + 1 == text.Length)
+        {
+            Console.WriteLine(text);
+            return 1;
+        }
+
+        int count = 0;
+        for (int withInd = at; withInd < text.Length; withInd++)
+        {
+            string swapedText = Swap(text, at, withInd);
+            count += Permutate(swapedText, at + 1);
+        }
+
+        return count;
+    }
+
+    static string Swap(string text, int at, int withInd)
+    {
+        char[] textArr = text.ToCharArray();
+
+        var swapedElement = textArr[at];
+        textArr[at] = textArr[withInd];
+        textArr[withInd] = swapedElement;
+
+        return new string(textArr);
+    }
+}
